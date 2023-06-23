e4ab182b8f3ddd3f28a217fad844a0908174b1c8
Ninok <nina_9a@mail.ru>
2023-06-23 15:48:01 +0300

    Залив проекта на гитхаб

---
+++ src/main/java/com/javarush/domain/City.java
@@ -0,0 +1,61 @@
+package com.javarush.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String district;
+
+    private Integer population;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
---
+++ src/main/java/com/javarush/domain/CountryLanguage.java
@@ -0,0 +1,68 @@
+package com.javarush.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Type;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "country_language")
+public class CountryLanguage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String language;
+
+// проверить будет ли булево поле маппиться без указания типа hibernate
+//    @Column(name = "is_official")
+    @Column(name = "is_official", columnDefinition = "BIT")
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    private Boolean isOfficial;
+
+    private BigDecimal percentage;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Boolean getOfficial() {
+        return isOfficial;
+    }
+
+    public void setOfficial(Boolean official) {
+        isOfficial = official;
+    }
+
+    public BigDecimal getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(BigDecimal percentage) {
+        this.percentage = percentage;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
